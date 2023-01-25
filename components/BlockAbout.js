1c5dd37a2ad94b80eb2ad65b5e06090c1d56a034
Kichuk Rostyslav <ros.kichuk@gmail.com>
2023-01-25 17:19:56 +0200

    change name block aboutt

--- components/BlockAbout.js
+++ components/BlockAbout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from '../styles/BlockAbout.module.scss'
+import style from '../styles/Blockabout.module.scss'
 import { motion } from 'framer-motion';
 
 const textSFirstAnimation = {
